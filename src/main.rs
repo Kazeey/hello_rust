a44f5d83d07ab1c728c0426408ec1b8118f47ed3
Kazeey <44819132+Kazeey@users.noreply.github.com>
2021-09-06 22:53:45 +0200

    Ajout génération random number

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use ferris_says::say;
 use std::io::{ stdout, BufWriter, stdin };
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() 
 {
@@ -18,6 +20,7 @@ fn ferris_says()
     .expect("Erreur");
 
     let mut out = String::from("Salut ");
+
     out.push_str(&name.to_string());
     
     let width = 24;
@@ -29,13 +32,25 @@ fn ferris_says()
 
 fn guessing_game()
 {
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("{}", secret_number);
     println!("Guess the number\nEnter your number");
 
     let mut guess = String::new();
-
+    
     stdin()
     .read_line(&mut guess)
     .expect("Erreur");
 
+    let guess : u32 = guess.trim().parse().expect("Please type a number !");
+
     println!("{}", guess);
+
+    match guess.cmp(&secret_number) 
+    {
+        Ordering::Less => println!("Too small"),
+        Ordering::Equal => println!("You win !"),
+        Ordering::Greater => println!("Too big")
+    }
 }

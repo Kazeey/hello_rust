fba7db44fb71135407679deeaf272c3d3541852b
Quentin Pezé <44819132+Kazeey@users.noreply.github.com>
2023-07-22 11:49:16 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,67 @@
-mod lib
-{
-    pub mod screen;
-    pub mod button;
+use std::fmt::Debug;
+
+#[derive(Debug)]
+struct Personnage {
+    nom : String,
+    age : u8,
+    level : u8,
+    vie : u32,
+    force : u32,
+    energie : u32,
+    etat : String
 }
 
-mod examples 
-{
-    pub mod ferris_says;
-    pub mod boucle;
-    pub mod guessing_game;
+enum MonErreur {
+    Indisponible,
+    Autre(String),
 }
 
-use lib::button;
-use lib::screen;
+impl Drop for Personnage {
+    fn drop(&mut self) {
+        println!("Le personnage est mort");
+    }
+}
 
+impl Personnage {
+    fn new (nom: String) -> Personnage
+    {
+        Personnage {
+            nom : nom,
+            age : 10,
+            level : 1,
+            vie : 10,
+            force : 10,
+            energie : 10,
+            etat : String::from("vivant")
+        }
+    }
+
+    fn rename (&mut self, name: String) -> &mut Personnage
+    {
+        self.nom = name;
+        return self;
+    }
+    
+    fn levelUp (&mut self) -> &mut Personnage
+    {
+        self.level += 1;
+        return self;
+    }
+
+    fn die (&mut self)
+    {
+        drop(self);
+    }
+}
 
 fn main() 
 {
-    screen::Draw::draw(&button::Button{width: 10, height: 10, label: String::from("Button")});
-    examples::guessing_game::guessing_game();
+    let personnage: Personnage = Personnage::new(String::from("Quentin"));
+    {
+        let mut personnage = personnage;
+        println!("{:?}", personnage);
+        personnage.levelUp();
+        println!("{:?}", personnage);
+        personnage.die();
+    }    
 }
